29bcabcb24d942b3d37b10de9b1670922c74c77b
Akshay Morey <akshay.morey@nagarro.com>
2017-08-30 11:11:35 +0530

    morgan logging added

--- Assignment1/DaysOnPlanet/index.js
+++ Assignment1/DaysOnPlanet/index.js
@@ -2,9 +2,15 @@ var express = require("express");
 var body_parser = require("body-parser");
 var fs = require("fs");
 var nd = require("./numdays.js");
+var morgan = require("morgan");
+var path = require("path");
 
 var app = express();
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
 app.use(body_parser.urlencoded({extended: false}));
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use("/", express.static(__dirname));
 app.use("/form", express.static(__dirname+"/assets/form.html"));
