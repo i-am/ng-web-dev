4fe660175d14f52d7f9194a7422573a0a874c6bd
Akshay Morey <akshay.morey@nagarro.com>
2017-09-02 17:06:53 +0530

    hide/show link added

--- TodoApp/public/script.js
+++ TodoApp/public/script.js
@@ -6,7 +6,43 @@ const TODO_TITLE = "new_todo_title";
 const COMPLETE_TODO_LIST_ID = "complete_todo_list_div";
 const DELETED_TODO_LIST_ID = "deleted_todo_list_div";
 
-window.onload = GetTodoAJAX();
+window.onload = function() {
+    GetTodoAJAX();
+    var complete_hide_link = document.getElementById("hide_completed");
+    var delete_hide_link = document.getElementById("hide_deleted");
+    complete_hide_link.onclick = function(e){
+        e.preventDefault();
+        ToggleTodosDisplay(COMPLETE_TODO_LIST_ID);
+        ToggleLinkText("hide_completed");
+        return true;
+    };
+
+    delete_hide_link.onclick = function(e){
+        e.preventDefault();
+        ToggleTodosDisplay(DELETED_TODO_LIST_ID);
+        ToggleLinkText("hide_deleted");
+    };
+};
+
+function ToggleLinkText(linkId)
+{
+    link_el = document.getElementById(linkId);
+    if(link_el)
+    if(link_el.innerHTML === "Hide Items")
+        link_el.innerHTML = "Show Items";
+    else link_el.innerHTML = "Hide Items";
+}
+
+function ToggleTodosDisplay(id) {
+    hide_div = document.getElementById(id);
+
+    for(var i=0; i<hide_div.children.length; i++)
+    {
+        if(hide_div.children[i].style.display !== 'none')
+            hide_div.children[i].style.display = 'none';
+        else hide_div.children[i].style.display = 'block';
+    }
+}
 
 function show_todo_elements(todo_data_json) {
     var todos = JSON.parse(todo_data_json);
@@ -47,15 +83,20 @@ function createTodoElement(key, todo) {
     if(todo.status !== "DELETED") {
         //checkbox
         var complete_checkbox = document.createElement("input");
-        complete_checkbox.type = "checkbox";
         complete_checkbox.setAttribute("class", "completeCheckbox");
+        complete_checkbox.type = "checkbox";
         complete_checkbox.setAttribute("onclick", "completeTodoAJAX(" + key + ")");
         if (todo.status === "COMPLETE") {
             complete_checkbox.setAttribute("checked", "true");
         }
         todo_element.appendChild(complete_checkbox);
-        todo_element.innerHTML += "<button type = 'button' class = 'close' onclick='deleteTodoAJAX(\""+key+"\")'>&times;</button>"
-
+        //todo_element.innerHTML += "<button type = 'button' class = 'close' onclick='deleteTodoAJAX(\""+key+"\")'>&times;</button>"
+        var del_button = document.createElement('button');
+        del_button.setAttribute('type', 'button');
+        del_button.setAttribute('class', 'close');
+        del_button.setAttribute('onclick', 'deleteTodoAJAX(' + key + ')');
+        del_button.innerHTML = '×';
+        todo_element.appendChild(del_button);
     }
     todo_element.append(todo.title);
 
@@ -68,7 +109,7 @@ function GetTodoAJAX(){
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState === RESPONSE_DONE && xhr.status === STATUS_OK) {
-            console.log(xhr.responseText);
+            // console.log(xhr.responseText);
             show_todo_elements(xhr.responseText);
         }
     };
@@ -83,7 +124,7 @@ function AddTodoAJAX() {
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === RESPONSE_DONE && xhr.status === STATUS_OK) {
-            console.log(xhr.responseText);
+            // console.log(xhr.responseText);
             show_todo_elements(xhr.responseText);
         }
     };
@@ -99,7 +140,7 @@ function completeTodoAJAX(id) {
         //code that needs to be run after response arrives
 
         if (xhr.readyState === RESPONSE_DONE && xhr.status === STATUS_OK) {
-                console.log(xhr.responseText);
+                // console.log(xhr.responseText);
                 show_todo_elements(xhr.responseText);
             }
         };
@@ -115,7 +156,7 @@ function deleteTodoAJAX(id) {
         //code that needs to be run after response arrives
 
         if (xhr.readyState === RESPONSE_DONE && xhr.status === STATUS_OK) {
-            console.log(xhr.responseText);
+            // console.log(xhr.responseText);
             show_todo_elements(xhr.responseText);
         }
     };
